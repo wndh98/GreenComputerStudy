0e20f78710c99974077e23eabe5f1e4424e43ca6
wndh98 <wndh98@naver.com>
2024-04-26 17:09:51 +0900

    test

---
+++ calculatorProject/calculator.js
@@ -0,0 +1,9 @@
+let resultInput = document.result;
+let 
+function checkInput(input){
+
+}
+function resetBtn() {}
+function opClick(op) {}
+function numberClick(num) {}
+function calculator() {}
---
+++ web/javascript/15_ecma/math.js
@@ -0,0 +1,4 @@
+export let sum = (x, y) => {
+    return x*y;
+}
+export let pi = 3.14;
